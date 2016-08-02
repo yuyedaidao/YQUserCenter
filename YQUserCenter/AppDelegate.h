bced8049bb07f74b1c4726a709bd7a81a6f79729
padding <wyqpadding@gmail.com>
2016-08-02 14:59:42 +0800

    Initial Commit

---
+++ YQUserCenter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YQUserCenter
+//
+//  Created by Wang on 16/8/2.
+//  Copyright © 2016年 Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YQUserCenter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YQUserCenter
+//
+//  Created by Wang on 16/8/2.
+//  Copyright © 2016年 Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
